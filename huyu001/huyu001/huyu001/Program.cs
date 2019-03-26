70799747024c9e40a11372355fb5d74a855c2785
huyu001 <214780419@qq.com>
2019-03-26 22:21:41 +0800

    Message

---
+++ huyu001/huyu001/huyu001/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApplication3
+{
+     public class Program
+    {
+       public static void Main(string[] args)
+        {
+            Console.WriteLine("第一个数：");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("输入运算符（+—*/）");
+            // c=Console.ReadLine();
+            char str = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("第er个数：");
+            int b = Int32.Parse(Console.ReadLine());
+            //int c = a + b;
+            //Console.WriteLine("两个数和为：{0}",c);
+            switch (str)
+            {
+                case '+':
+                    Console.WriteLine("两个数和为：{0}", a + b);
+                    break;
+                case '-':
+                    Console.WriteLine("两个数差为：{0}", a - b);
+                    break;
+                case '*':
+                    Console.WriteLine("两个数积为：{0}", a * b);
+                    break;
+                case '/':
+                    Console.WriteLine("两个数商为：{0}", a / b);
+                    break;
+                
+                default:
+                    Console.WriteLine("输入正确的运算符！！");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ huyu001/huyu001/huyu001Tests1/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace huyu001Tests1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine("第一个数：");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("输入运算符（+—*/）");
+            // c=Console.ReadLine();
+            char str = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("第er个数：");
+            int b = Int32.Parse(Console.ReadLine());
+            //int c = a + b;
+            //Console.WriteLine("两个数和为：{0}",c);
+            switch (str)
+            {
+                case '+':
+                    Console.WriteLine("两个数和为：{0}", a + b);
+                    break;
+                case '-':
+                    Console.WriteLine("两个数差为：{0}", a - b);
+                    break;
+                case '*':
+                    Console.WriteLine("两个数积为：{0}", a * b);
+                    break;
+                case '/':
+                    Console.WriteLine("两个数商为：{0}", a / b);
+                    break;
+
+                default:
+                    Console.WriteLine("输入正确的运算符！！");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
